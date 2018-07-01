ce07b20cc134a01d16306caf9b23c8b4619b133a
twangcode <40374008+twangcode@users.noreply.github.com>
2018-06-30 23:01:58 -0500

    Chapter 4

--- Chapter 4/BasicDataTypes.py
+++ Chapter 4/BasicDataTypes.py
@@ -1,3 +1,6 @@
+############
+# int type
+############
 a = 10
 print type(a)
 print a.bit_length()
@@ -6,3 +9,42 @@ print b.bit_length()
 c = 10 ** 100
 print c
 print c.bit_length()
+
+##################
+# float type
+##################
+a = 1. / 4
+print type(a)
+b = 0.35
+print b + 0.1
+print b.as_integer_ratio()
+
+import decimal
+from decimal import Decimal 
+
+print decimal.getcontext()
+print Decimal(1) / Decimal(11)
+
+decimal.getcontext().prec = 4
+print Decimal(1) / Decimal(11)
+
+decimal.getcontext().prec = 50
+print Decimal(1) / Decimal(11)
+
+#################
+# strings
+#################
+t = 'this is a string object'
+print t.capitalize()
+print t.split()
+print 'http://www.python.org'.strip('htp:/')
+print 'lalalalala'.count('la')
+
+import re
+series = """
+'01/18/2014 13:00:00', 100, '1st';
+'01/18/2014 13:30:00', 110, '2nd';
+'01/18/2014 14:00:00', 120, '3rd'
+"""
+dt = re.compile("'[0-9/:\s]+'")
+print dt.findall(series)
